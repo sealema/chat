9074f39275fb28e90948f4950bb48602e5c165d9
sealema <54751372+sealema@users.noreply.github.com>
2019-08-31 21:26:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ var express = require('express')
 , _ = require('underscore')._;
 
 app.configure(function() {
-	app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
-  	app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+	app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 5000);
+  	app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "142.93.182.190");
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(express.static(__dirname + '/public'));
@@ -33,8 +33,8 @@ app.get('/', function(req, res) {
   res.render('index.html');
 });
 
-server.listen(app.get('port'), app.get('ipaddr'), function(){
-	console.log('Express server listening on  IP: ' + app.get('ipaddr') + ' and port ' + app.get('port'));
+server.listen(app.get('port'), app.get('ipaddr')"142.93.182.190", function(){
+	console.log('Express server listening on  port ' + app.get('port'));
 });
 
 io.set("log level", 1);
